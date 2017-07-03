34170856d3c64c52e293281f72a542a79c111fe0
th3w01f <opensystem.992@gmail.com>
2017-07-03 00:51:47 -0400

    update primes

--- primes.py
+++ primes.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
 """
-Your module documentation here
+Modulo que retorna si el nro que se le pasa es primo o no
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Clase que recibe un objeto y con una sola funcion
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        funcion que analiza primero si es menor que 2
+        (el 1 no se toma ni como primo ni como no primo)
+        luego lo hace una division entera y verifica
+        si el resultado del resto es 0
         """
-        prime = True
+        primo = True
         if num_int < 2:
-            prime = False
+            primo = False
         else:
             num = num_int // 2
-            for iteration in range(2, num + 1):
-                if num_int % iteration == 0:
-                    prime = False
-        return prime
+            for it in range(2, num + 1):
+                if num_int % it == 0:
+                    primo = False
+        return primo
